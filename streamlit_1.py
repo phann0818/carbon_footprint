5a0a765c0fb69294acc9547c83f524830c31d2c8
phann0818 <152852189+phann0818@users.noreply.github.com>
2023-12-29 21:25:47 +0700

    Update streamlit_1.py

--- streamlit_1.py
+++ streamlit_1.py
@@ -127,18 +127,17 @@ total_emissions = round(
 )
 comparison = round((total_emissions/average_emissions)*100,2)
 
-# # Create a centered container for the button
-# with st.container():
-#     st.session_state.button_clicked = st.button(
-#         "Calculate CO2 Emissions",
-#         key="calculate_button",  # Add a key to avoid unnecessary re-runs
-#         # Increase button size and center it horizontally
-#         width=200,  # Adjust width as needed
-#         align="center"
-#     )
+# Create a centered container for the button
+with st.container():
+    st.session_state.button_clicked = st.button(
+        "Calculate CO2 Emissions",
+        key="calculate_button",  # Add a key to avoid unnecessary re-runs
+        # Increase button size and center it horizontally
+        width=200,  # Adjust width as needed
+        align="center"
+    )
 
-if st.button("Calculate CO2 Emissions", width=200, align="center"):
-
+if st.session_state.button_clicked:
     # Display results
     st.header("Results")
     
